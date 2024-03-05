fa291bc3e74843435165eca43c9aac4e208a43a0
ZachMommaerts <zmommaerts@gmail.com>
2024-03-04 19:41:44 -0600

    Initial Commit

---
+++ Layout_Geometry_Technique_Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Layout_Geometry_Technique_Project
+//
+//  Created by Zach Mommaerts on 3/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Layout_Geometry_Technique_Project/Layout_Geometry_Technique_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Layout_Geometry_Technique_ProjectApp.swift
+//  Layout_Geometry_Technique_Project
+//
+//  Created by Zach Mommaerts on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Layout_Geometry_Technique_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
