30f7114ffded7a3242d7b52f7a63bb4f874bdc88
ZachMommaerts <zmommaerts@gmail.com>
2024-03-09 16:45:23 -0600

    - Complete challenge

---
+++ Layout_Geometry_Technique_Project/Challenge.swift
@@ -0,0 +1,18 @@
+//
+//  Challenge.swift
+//  Layout_Geometry_Technique_Project
+//
+//  Created by Zach Mommaerts on 3/9/24.
+//
+
+import SwiftUI
+
+struct Challenge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Challenge()
+}
